ddd3b0fb74a627515c2b7c95a923e269c14358c6
EHTISHAM-Afzal <106005092+EHTISHAM-Afzal@users.noreply.github.com>
2024-01-16 23:14:28 +0500

    Add files via upload

---
+++ app/TechStack/page.tsx
@@ -0,0 +1,42 @@
+import GlowingCardsProvider from "@/components/Cards/GlowingCardsProvider";
+import TechCard from "@/components/Cards/TechCard";
+import { Separator } from "@/components/ui/separator";
+import { AppLogos, CategorizedAppLogos } from "@/lib/Data";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      {/* Header */}
+      <header className=" mb-16 mt-36 md:mt-52">
+        <h1 className=" break-words text-foreground m-1 text-3xl font-bold leading-tight tracking-tight md:text-6xl lg:leading-[1.1]">
+          Tech Stack
+        </h1>
+        <p className="mb-16 mt-4 md:text-2xl pl-2 text-lg text-muted-foreground sm:text-xl">
+          The dev tools, Softwares, I use
+        </p>
+      </header>
+
+      <Separator className="my-6" />
+
+      <h2 className=" text-foreground md:text-4xl scroll-m-20 text-2xl font-bold tracking-tight">
+        Dev & Design
+      </h2>
+      <GlowingCardsProvider>
+        <div className="w-full my-5 grid grid-cols-1 gap-4 md:my-8 md:grid-cols-3">
+          {AppLogos.map((logo) => (
+            <TechCard
+              key={logo.Name}
+              ImageURL={logo.ImageURL}
+              href={logo.href}
+              Name={logo.Name}
+              Tage={logo.Tage}
+            />
+          ))}
+        </div>
+      </GlowingCardsProvider>
+    </div>
+  );
+};
+
+export default page;

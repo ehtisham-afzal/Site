e069bc5f770bec5012b3b6bd353f7040a7fb6f97
EHTISHAM-Afzal <khaj3172@gmail.com>
2024-01-19 18:13:22 +0500

    corrected the TechCard glow effect padding 	modified:   src/components/Cards/TechCard.tsx

--- src/components/Cards/TechCard.tsx
+++ src/components/Cards/TechCard.tsx
@@ -19,8 +19,8 @@ const TechCard = ({
       className="group relative flex items-center justify-center gap-4  p-4 transition duration-100 md:h-[300px] md:p-6  hover:bg-card-foreground/10"
     >
       {/* Side glow Effect */}
-      <span className="absolute w-px left-0 top-[50%] h-[40%] bg-gradient-to-b from-blue-500/0 via-blue-500/40 to-blue-500/0 dark:from-blue-400/0 dark:via-blue-400/40 dark:to-blue-400/0" />
-      <span className="absolute w-[40%] top-0 right-px h-px bg-gradient-to-r from-blue-500/0 via-blue-500/40 to-blue-500/0 dark:from-blue-400/0 dark:via-blue-400/40 dark:to-blue-400/0" />
+      <span className="absolute w-px -left-px top-[50%] h-[40%] bg-gradient-to-b from-blue-500/0 via-blue-500/40 to-blue-500/0 dark:from-blue-400/0 dark:via-blue-400/40 dark:to-blue-400/0" />
+      <span className="absolute w-[40%] -top-px right-px h-px bg-gradient-to-r from-blue-500/0 via-blue-500/40 to-blue-500/0 dark:from-blue-400/0 dark:via-blue-400/40 dark:to-blue-400/0" />
 
       <div className="relative block h-[60px] w-[60px] origin-bottom transition duration-300 md:h-[100px] md:w-[100px] md:group-hover:-translate-y-1 md:group-hover:scale-[101%] backdrop-blur-sm rounded-xl bg-foreground/5 group-hover:bg-foreground/10 p-2">
         <Image

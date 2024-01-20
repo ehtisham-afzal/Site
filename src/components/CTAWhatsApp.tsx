ac89d71364951e731ba7e5e5d119a0ae0fc4a9d3
EHTISHAM-Afzal <khaj3172@gmail.com>
2024-01-20 13:00:08 +0500

    Changes to be committed: 	modified:   src/components/CTAWhatsApp.tsx

--- src/components/CTAWhatsApp.tsx
+++ src/components/CTAWhatsApp.tsx
@@ -29,16 +29,16 @@ const CTAWhatsApp = () => {
             type="submit"
           >
             <svg
-            className="h-full px-1"
+              className="h-full px-1"
               xmlns="http://www.w3.org/2000/svg"
               width="24"
               height="24"
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
               <path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z" />
               <path d="m10 10-2 2 2 2" />

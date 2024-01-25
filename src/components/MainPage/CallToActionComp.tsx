b2aa268a79c580627d9074d78b59fce17f530c5d
EHTISHAM-Afzal <khaj3172@gmail.com>
2024-01-25 08:58:46 +0500

    Changes to be committed: 	modified:   src/components/MainPage/CallToActionComp.tsx

--- src/components/MainPage/CallToActionComp.tsx
+++ src/components/MainPage/CallToActionComp.tsx
@@ -5,10 +5,10 @@ const CallToActionComp = () => {
   return (
     <section className="relative z-10 flex flex-col items-center justify-between gap-10 p-4 border rounded-xl bg-secondary/50 my-28 lg:flex-row">
       <div>
-        <h2 className="text-3xl font-bold text-center lg:text-left lg:text-5xl">
+        <h2 className="text-3xl font-bold text-center lg:text-left lg:text-4xl">
           Let’s work together
         </h2>
-        <p className="mt-4 text-xl text-center text-muted-foreground lg:text-left text-pretty">
+        <p className="mt-4 text-lg md:text-xl text-center text-muted-foreground lg:text-left text-pretty">
           Want to discuss an opportunity to create something great
           <br className="hidden lg:inline-block" /> I’m ready when you are.
         </p>

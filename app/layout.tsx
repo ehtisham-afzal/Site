94d8a685cabbafdfbea9e9cda2c5de366088f6db
EHTISHAM-Afzal <khaj3172@gmail.com>
2024-01-26 18:55:59 +0500

    fixed global.css path Changes to be committed: 	modified:   app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
-import "@/Styles/globals.css";
+import "../Styles/globals.css";
 import { cn } from "../lib/utils";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Nav from "@/components/NavBar/Nav";

b52ea162a7464fba635483d51ea104237c4d8fc8
EHTISHAM-Afzal <khaj3172@gmail.com>
2024-02-04 09:41:17 +0500

    Changes to be committed: 	modified:   app/Projects/LittleLemon/layout.tsx 	modified:   app/Projects/LittleLemon/page.mdx

--- app/Projects/LittleLemon/layout.tsx
+++ app/Projects/LittleLemon/layout.tsx
@@ -1,7 +1,7 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   // Create any shared layout or styles here
   return (
-    <div className="prose min-w-full dark:text-accent-foreground/90 prose-indigo px-px prose-strong:text-purple-500 prose-headings:text-accent-foreground flex justify-center prose-lg md:prose-2xl w-full h-full mt-48">
+    <div className="prose min-w-full dark:text-accent-foreground/90 prose-indigo px-px prose-strong:text-secondary-foreground prose-headings:text-accent-foreground flex justify-center prose-lg md:prose-2xl w-full h-full mt-48">
       <div>{children}</div>
     </div>
   );

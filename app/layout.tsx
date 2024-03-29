15a86dea1459a12c1110199549e55b94eda394d0
EHTISHAM-Afzal <khaj3172@gmail.com>
2024-03-29 16:45:52 +0500

    Worked on SEO optimization changed the whole architicture of Metadata Changes to be committed: 	modified:   app/layout.tsx 	deleted:    app/opengraph-image.jpg 	new file:   config/site.ts 	new file:   public/android-chrome-192x192.png 	new file:   public/android-chrome-512x512.png 	new file:   public/apple-touch-icon.png 	new file:   public/favicon-16x16.png 	new file:   public/favicon-32x32.png 	new file:   public/favicon.ico 	new file:   public/og.jpg 	new file:   public/site.webmanifest 	new file:   public/sitemap.xml 	new file:   public/urllist.txt 	new file:   robots.ts 	renamed:    app/robots.txt -> robots.txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,14 +5,65 @@ import { cn } from "./lib/utils";
 import { ThemeProvider } from "./ui/ThemeProvider";
 import Nav from "./ui/NavBar/Nav";
 import Footer from "./ui/Footer";
+import { siteConfig } from "../config/site";
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | Ehtisham Afzal Jr Web Dev",
-    default: "Ehtisham Afzal Jr Web Dev",
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
   },
-  description: "Ehtisham Afzal Personal portfolio Site",
-  metadataBase: new URL("https://ehtisham.dev"),
+  metadataBase: new URL(siteConfig.url),
+  description: siteConfig.description,
+  keywords: [
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "ehtisham",
+    "afzal",
+    "portfolio",
+    "webdev",
+    "developer",
+    "programmer",
+    "software engineer",
+    "software developer",
+    "web developer",
+  ],
+  authors: [
+    {
+      name: "ehtisham afzal",
+      url: "https://ehtisham.dev",
+    },
+  ],
+  creator: "ehtisham afzal",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@ehtisham_dev",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
 };
 
 const fontSans = FontSans({
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "Ehtisham Afzal Web Dev",
+    url: "https://ehtisham.dev",
+    ogImage: "https://https://www.ehtisham.dev/og.jpg",
+    description: "Ehtisham Afzal Site",
+    links: {
+        linkedIn: "https://www.linkedin.com/in/ehtisham-afzal/",
+        github: "https://github.com/ehtisham-afzal",
+        twitter: "https://twitter.com/ehtisham_dev",
+    },
+}
+
+export type SiteConfig = typeof siteConfig

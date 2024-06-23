6a05cfdce7e28522938ccda158bd24900b67b305
EHTISHAM-Afzal <khaj3172@gmail.com>
2024-06-23 10:58:30 +0500

    changed my picture
    Changes to be committed:
    	modified:   app/ui/Cards/TechCard.tsx
    	modified:   public/ehtisham.jpg

--- app/ui/Cards/TechCard.tsx
+++ app/ui/Cards/TechCard.tsx
@@ -29,7 +29,7 @@ const TechCard = ({
           src={ImageURL}
           width={100}
           height={100}
-          className="w-full h-full rounded-lg overflow-hidden"
+          className="w-full h-full object-contain"
         />
       </div>
       <a
